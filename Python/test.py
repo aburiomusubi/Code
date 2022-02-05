70158e6023c613831b7963ead5eea086d2e5ffd9
aburiomusubi <aburiomusubi@gmail.com>
2022-02-05 16:17:06 +0900

    omusubi_code

--- Python/test.py
+++ Python/test.py
@@ -1,1 +1,14 @@
-print("Hello")
+def aburi_omusubi(omu,kome,nori):
+  for i in range(omu):
+    if kome[i] == "aburi" and nori[i] == "aburi":
+      print("aburi_omusubi")
+    elif kome[i] == "normal" and nori[i] == "normal":
+      print("omusubi")
+    else:
+      print("half_aburi_omusubi")
+
+omu = int(input())
+kome = list(map(str,input().split()))
+nori = list(map(str,input().split()))
+aburi_omusubi(omu,kome,nori)
+
